c468f96355a161bd02921e83e916f4d8e9f54f20
JaRP <100147964+Wooloowooloo@users.noreply.github.com>
2025-01-21 08:04:27 +0800

    Add coyote time

--- Assets/Scripts/PlayerMovement/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -1,24 +1,36 @@
-using System.Threading;
 using UnityEngine;
 using UnityEngine.InputSystem; 
+
 public class PlayerMovement: MonoBehaviour
 {
+    [Header("Components")]
     public Rigidbody2D rb;
     public Transform groundCheck;
     public LayerMask groundLayer;
 
-    private float speed = 8f;
+    [Header("Horizontal Movement Configs")]
+    [SerializeField] private float speed = 8f;
     private float horizontal;
-    private float jumppower = 5f;
     private bool isFacingRight = true;
-
+    
+    [Header("Jump Configs")]
+    [SerializeField] private float jumpPower = 5f;
+    [SerializeField, Range(0.05f, 0.25f)] private float coyoteTime = 0.2f;
+    [SerializeField, Range(0.05f, 0.25f)] private float jumpBufferTime = 0.2f;
+    private float lastGroundedTime = 0f;
+    
+    [Header("Debug Configs")]
+    [SerializeField] private bool showGizmos = false;
+    
     private void Update()
     {
         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y); 
         if(!isFacingRight && horizontal > 0f) { Flip(); }
         else if(isFacingRight && horizontal < 0f) { Flip(); } 
+        
+        //Handles the timer for coyote time
+        lastGroundedTime = IsGrounded() ?  0f : lastGroundedTime += Time.deltaTime;
     }
-
 
     private bool IsGrounded()
     {
@@ -27,9 +39,17 @@ public class PlayerMovement: MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if(context.performed && IsGrounded()) 
-        { rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumppower); }
-        if(context.canceled && rb.linearVelocity.y > 0 ) { new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f); }
+        /*if(context.performed && IsGrounded()) 
+        { rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower); }
+        if(context.canceled && rb.linearVelocity.y > 0 ) { new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f); }*/
+
+        //OPTIMIZE: validates if player can jump;
+        bool canJump = IsGrounded() || (!IsGrounded() && lastGroundedTime < coyoteTime);
+        
+        if (context.performed && canJump)
+        {
+            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
     }
     private void Flip()
     {
@@ -42,5 +62,16 @@ public class PlayerMovement: MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<Vector2>().x; 
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!showGizmos)
+        {
+            return;
+        }
+        
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, 0.2f);
     }
 }
