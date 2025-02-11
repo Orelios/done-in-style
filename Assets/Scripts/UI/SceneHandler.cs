d4a49f215cc8813dd0a279848253a8169b2458d8
SCDN01 <116152025+SCDN01@users.noreply.github.com>
2025-02-11 15:18:31 +0800

    Implement lose and win state

---
+++ Assets/Scripts/UI/SceneHandler.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public void RestartScene() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitApllication()
+    {
+        Application.Quit();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.GetSceneByBuildIndex(SceneManager.sceneCountInBuildSettings + 1);
+    }
+
+    public void LoadPreviousScene()
+    {
+        SceneManager.GetSceneByBuildIndex(SceneManager.sceneCountInBuildSettings - 1);
+    }
+}
