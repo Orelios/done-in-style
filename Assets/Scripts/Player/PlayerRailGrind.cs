5e78728ee1f96f0d193943763caa825c2ace413c
JaRP <100147964+Wooloowooloo@users.noreply.github.com>
2025-02-13 13:23:00 +0800

    Implement railing momentum decay and tilt player on railing

--- Assets/Scripts/Player/PlayerRailGrind.cs
+++ Assets/Scripts/Player/PlayerRailGrind.cs
@@ -6,19 +6,22 @@ public class PlayerRailGrind : MonoBehaviour
     [Header("Grinding Configs")]
     [SerializeField] private float grindingSpeedMultiplier;
     [SerializeField] private float minimumSpeedToGrind;
-    [SerializeField] private Vector2 grindMomentumDecay = new Vector2(0.345f, 0.69f);
     public float MinimumSpeedToGrind => minimumSpeedToGrind;
-    [SerializeField] private float heightOffset;
+    [SerializeField] private Vector2 momentumDecay = new Vector2(0.345f, 0.69f);
+    
     public bool IsOnRail;
     private float _railDirection;
     private float _grindingSpeed;
+    private Quaternion _rotationBeforeGrinding;
+    private SpriteRenderer _playerSprite;
     
     private Rigidbody2D _rb;
     private Railing _railing;
 
-    private void Start()
+    private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _playerSprite = transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
@@ -33,22 +36,24 @@ public class PlayerRailGrind : MonoBehaviour
     {
         _rb.linearVelocity = new Vector2(_grindingSpeed, _rb.linearVelocity.y);
     }
-
+    
     private IEnumerator DecayMomentum()
     {
-        var lastVelocity = _rb.linearVelocity;
+        Vector2 lastVelocity = _rb.linearVelocity;
 
-        _rb.linearVelocity = new(lastVelocity.x > 0f ? lastVelocity.x - grindMomentumDecay.x : lastVelocity.x + grindMomentumDecay.x, lastVelocity.y);
-
+        _rb.linearVelocity = new(lastVelocity.x > 0 ? lastVelocity.x - momentumDecay.x : lastVelocity.x + momentumDecay.x, lastVelocity.y - momentumDecay.y);
+        
         yield return null;
     }
 
     public void EnableRailing(Railing railing)
     {
+        _rotationBeforeGrinding = transform.rotation;
         IsOnRail = true;
         _railing = railing;
         _grindingSpeed = _rb.linearVelocityX * grindingSpeedMultiplier;
         _railDirection = transform.rotation.y == 0 ? 1 : -1;
+        _playerSprite.gameObject.transform.rotation = Quaternion.Euler(transform.localEulerAngles.x, transform.localEulerAngles.y, Mathf.Approximately(_railDirection, 1) ? railing.transform.localEulerAngles.z : -railing.transform.localEulerAngles.z);
     }
 
     public void DisableRailing()
@@ -57,7 +62,7 @@ public class PlayerRailGrind : MonoBehaviour
         _railDirection = 0f;
         IsOnRail = false;
         _railing = null;
-        //_rb.linearVelocity = new(_rb.linearVelocityX / 2f, _rb.linearVelocityY);
+        _playerSprite.gameObject.transform.rotation = _rotationBeforeGrinding;
         StartCoroutine(DecayMomentum());
     }
 }
