eb738d121f2d56772b8319cf7c2bd30625af93e1
vongabrieltan <118342428+vongabrieltan@users.noreply.github.com>
2025-02-21 16:06:43 +0800

    Add onomatopoeia

--- Assets/Scripts/Obstacles/DestructibleObjects.cs
+++ Assets/Scripts/Obstacles/DestructibleObjects.cs
@@ -2,6 +2,18 @@ using UnityEngine;
 
 public class DestructibleObjects : MonoBehaviour
 {
+    private Onomatopoeia _type;
+
+    private void Start()
+    {
+        _type = transform.parent.GetComponentInChildren<Onomatopoeia>();
+        #region Makes sure this Destructible Object is the one with a Canvas
+        if (_type == null)
+        {
+            Destroy(gameObject);
+        }
+        #endregion
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -9,9 +21,9 @@ public class DestructibleObjects : MonoBehaviour
             || playerTricks.IsPounding))
         {
             collision.gameObject.GetComponent<PlayerTricks>().AddScoreAndRank();
-            //collision.gameObject.GetComponent<PlayerTricks>().EnableTrick(gameObject);
             playerTricks.EnableTrickDestroyed();
             TimeHandler.SlowDownTime();
+            _type.StartTypingVFX();
             Destroy(gameObject);
         }
     }
@@ -22,11 +34,11 @@ public class DestructibleObjects : MonoBehaviour
         {
             if (collision.gameObject.GetComponent<PlayerTricks>().canTrick == false)
             {
-                //collision.gameObject.GetComponent<PlayerTricks>().EnableTrick(gameObject);
                 playerTricks.EnableTrickDestroyed();
             }
             TimeHandler.SlowDownTime();
             transform.GetChild(0).GetComponent<Collider2D>().enabled = false;
+            _type.StartTypingVFX();
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/VFX/Onomatopoeia.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Onomatopoeia : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> Letters = new List<Sprite>();
+    [SerializeField] private float typingSpeed = 0.25f;
+    [SerializeField] private float enlargeTime = 0.5f;
+    [SerializeField] private float smallScaleFactor = 0.5f;
+    [SerializeField] private float disableTime = 3.0f;
+    
+    private void Start()
+    {
+        //StartCoroutine(TypeImages());
+        GetComponent<Canvas>().worldCamera = Camera.main;
+        DisableChildren();
+    }
+
+    public void StartTypingVFX()
+    {
+        EnableChildren();
+        StartCoroutine(TypeImages());
+    }
+
+    private IEnumerator TypeImages()
+    {
+        Transform[] children = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            children[i] = transform.GetChild(i);
+            Color imageColor = children[i].GetComponent<Image>().color;
+            imageColor.a = 0f;
+            children[i].GetComponent<Image>().color = imageColor;
+        }
+
+        for (int i = 0; i < children.Length && i < Letters.Count; i++)
+        {
+            Image image = children[i].GetComponent<Image>();
+            if (image != null)
+            {
+                image.sprite = Letters[i];
+                StartCoroutine(AnimateSize(children[i], image));
+            }
+            yield return new WaitForSeconds(typingSpeed);
+        }
+
+        yield return new WaitForSeconds(disableTime);
+        foreach (Transform child in children)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+
+    private IEnumerator AnimateSize(Transform child, Image image)
+    {
+        Vector3 originalSize = child.localScale;
+        Vector3 smallSize = originalSize * smallScaleFactor;
+        float elapsedTime = 0f;
+
+        Color imageColor = image.color;
+        imageColor.a = 1f;
+        image.color = imageColor;
+
+        child.localScale = smallSize;
+        while (elapsedTime < enlargeTime)
+        {
+            child.localScale = Vector3.Lerp(smallSize, originalSize, elapsedTime / enlargeTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        child.localScale = originalSize;
+    }
+
+    private void DisableChildren()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+
+    private void EnableChildren()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+        }
+    }
+}
