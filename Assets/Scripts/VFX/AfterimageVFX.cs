f188a509ab203a7415747fa08001f2c862f41d4d
vongabrieltan <118342428+vongabrieltan@users.noreply.github.com>
2025-03-04 10:05:44 +0800

    Add multicolor afterimages

--- Assets/Scripts/VFX/AfterimageVFX.cs
+++ Assets/Scripts/VFX/AfterimageVFX.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class AfterimageVFX : MonoBehaviour
@@ -13,11 +14,14 @@ public class AfterimageVFX : MonoBehaviour
     private Transform afterimageParent;
 
     private Player _player;
+    private RankCalculator _rankCalculator;
+    [SerializeField] private List<Color> colorList = new List<Color>();
+    private int currentIndex = 1; // To track the current index in the list
+
 
     void Start()
     {
         _player = GetComponentInParent<Player>();
-
         rb = GetComponentInParent<Rigidbody2D>();
         //rb = _player.Rigidbody;
         spriteRenderer = transform.parent.GetChild(0).GetComponent<SpriteRenderer>();
@@ -29,11 +33,19 @@ public class AfterimageVFX : MonoBehaviour
             parentObj = new GameObject("Afterimages");
         }
         afterimageParent = parentObj.transform;
+
+        _rankCalculator = GameObject.Find("Score System").GetComponent<RankCalculator>();
+        colorList.Add(new Color(1f, 1f, 1f, 0f)); //invisible
+        colorList.Add(Color.red);
+        colorList.Add(new Color(1f, 0.647f, 0f));   // Orange (RGB)
+        colorList.Add(Color.yellow);
+        colorList.Add(Color.green);
+        colorList.Add(Color.blue);
     }
 
     void Update()
     {
-        if (Mathf.Abs(rb.linearVelocity.x) >= minSpeed && Time.time >= lastAfterimageTime + interval)
+        if (Mathf.Abs(rb.linearVelocity.x) >= minSpeed && Time.time >= lastAfterimageTime + interval && _rankCalculator.CurrentStylishRankIndex > 0)
         {
             CreateAfterimage();
             lastAfterimageTime = Time.time;
@@ -49,7 +61,13 @@ public class AfterimageVFX : MonoBehaviour
 
         SpriteRenderer afterimageRenderer = afterimage.AddComponent<SpriteRenderer>();
         afterimageRenderer.sprite = spriteRenderer.sprite;
-        afterimageRenderer.color = new Color(1f, 1f, 1f, 1f);
+        //afterimageRenderer.color = new Color(1f, 1f, 1f, 1f);
+        if (currentIndex > _rankCalculator.CurrentStylishRankIndex)
+        {
+            currentIndex = 1;
+        }
+        afterimageRenderer.color = colorList[currentIndex];
+        CycleColors();
         afterimageRenderer.sortingLayerID = spriteRenderer.sortingLayerID;
         afterimageRenderer.sortingOrder = spriteRenderer.sortingOrder - 1;
 
@@ -61,14 +79,29 @@ public class AfterimageVFX : MonoBehaviour
         float elapsedTime = 0f;
         Color startColor = renderer.color;
 
-        while (elapsedTime < lifetime)
+        if (renderer.color.a > 0)
         {
-            elapsedTime += Time.deltaTime;
-            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / lifetime);
-            renderer.color = new Color(startColor.r, startColor.g, startColor.b, alpha);
-            yield return null;
+            while (elapsedTime < lifetime)
+            {
+                elapsedTime += Time.deltaTime;
+                float alpha = Mathf.Lerp(1f, 0f, elapsedTime / lifetime);
+                renderer.color = new Color(startColor.r, startColor.g, startColor.b, alpha);
+                yield return null;
+            }
         }
+        
 
         Destroy(afterimage);
+    }
+
+    private void CycleColors()
+    {
+        currentIndex++;
+
+        // If the index reaches the end of the list, reset it to red
+        if (currentIndex >= colorList.Count)
+        {
+            currentIndex = 1;
+        }
     }
 }
