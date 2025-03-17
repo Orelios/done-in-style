776b5b7231d639708311f3ccb640a73b2c6f3854
SCDN01 <116152025+SCDN01@users.noreply.github.com>
2025-03-17 09:31:38 +0800

    Edit jump pad variation script

--- Assets/Scripts/Obstacles/JumpPadVariation.cs
+++ Assets/Scripts/Obstacles/JumpPadVariation.cs
@@ -4,10 +4,7 @@ public class JumpPadVariation : MonoBehaviour
 {
     [Header("Jump Pad Variation components")]
     [SerializeField] private Transform target;
-    [SerializeField] private string targetLayerName;
-    [SerializeField] private float bouncePadRange;
-    [SerializeField] private float bounceHeightX;
-    [SerializeField] private float bounceHeightY;
+    [SerializeField] private float bounceHeight;
     [SerializeField] private float maxHeight;
 
     private Vector2 _direction;
@@ -21,18 +18,13 @@ public class JumpPadVariation : MonoBehaviour
         Vector2 targetPos = target.position;
         _direction = targetPos - (Vector2)transform.position;
 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, _direction, bouncePadRange, LayerMask.GetMask(targetLayerName));
-
-        if (hit)
-        {
-            transform.up = _direction;   
-        }
+        //RaycastHit2D hit = Physics2D.Raycast(transform.position, _direction, bouncePadRange, LayerMask.GetMask(targetLayerName));
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(_direction * bounceHeightX, ForceMode2D.Impulse);
-        Debug.Log(_direction * bounceHeightX);
+        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(_direction * bounceHeight, ForceMode2D.Impulse);
+        Debug.Log(_direction * bounceHeight);
 
         collision.gameObject.GetComponent<Rigidbody2D>().linearVelocity =
                 Vector2.ClampMagnitude(collision.gameObject.GetComponent<Rigidbody2D>().linearVelocity, maxHeight);
