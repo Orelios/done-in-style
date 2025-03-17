8aa4caaeeb2cbc5b32c92a54851ead3da301e744
Patrick Jose Valenzuela <201901575@iacademy.edu.ph>
2025-03-17 14:06:39 +0800

    Update prototype levels and prefabs

--- Assets/Scripts/Obstacles/JumpPadVariation.cs
+++ Assets/Scripts/Obstacles/JumpPadVariation.cs
@@ -7,10 +7,17 @@ public class JumpPadVariation : MonoBehaviour
     [SerializeField] private float bounceHeight;
     [SerializeField] private float maxHeight;
 
+    public bool hasTricked = false;
+    private bool _hasGivenScore = false;
+    public Graffiti graffiti;
+    //public bool isOnJumpPad = false;
+
+    private PlayerTricks _playerTricks;
+
     private Vector2 _direction;
     private void Update()
     {
-        FindTrajectory(); 
+        FindTrajectory();
     }
 
     private void FindTrajectory()
@@ -21,12 +28,44 @@ public class JumpPadVariation : MonoBehaviour
         //RaycastHit2D hit = Physics2D.Raycast(transform.position, _direction, bouncePadRange, LayerMask.GetMask(targetLayerName));
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnCollisionStay2D(Collision2D collision)
     {
         collision.gameObject.GetComponent<Rigidbody2D>().AddForce(_direction * bounceHeight, ForceMode2D.Impulse);
         Debug.Log(_direction * bounceHeight);
 
         collision.gameObject.GetComponent<Rigidbody2D>().linearVelocity =
                 Vector2.ClampMagnitude(collision.gameObject.GetComponent<Rigidbody2D>().linearVelocity, maxHeight);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        AudioManager.instance.PlayOneShot(FMODEvents.instance.SpringBoard, this.transform.position);
+
+        if (collision.gameObject.TryGetComponent<PlayerTricks>(out var playerTricks))
+        {
+            _playerTricks = playerTricks;
+            if (!playerTricks.isOnJumpPad)
+            {
+                playerTricks.isOnJumpPad = true;
+                _playerTricks.CallJumpPadTimer();
+            }
+        }
+
+        if (!_hasGivenScore)
+        {
+            collision.gameObject.GetComponent<PlayerTricks>().AddScoreAndRank();
+            _hasGivenScore = true;
+            if (graffiti != null)
+            {
+                graffiti.StartGraffiti();
+            }
+        }
+        if (!hasTricked)
+        {
+            collision.gameObject.GetComponent<PlayerTricks>().DisableCanTrick();
+            collision.gameObject.GetComponent<PlayerTricks>().EnableTrick(gameObject);
+        }
+
     }
 }
