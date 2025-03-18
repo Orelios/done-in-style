3393c367503b04c21ee64fca9988f03441768e24
vongabrieltan <118342428+vongabrieltan@users.noreply.github.com>
2025-03-18 17:27:27 +0800

    Improve placement of railing 2.0

--- Assets/Scripts/MoveToPoints/MovePoint.cs
+++ Assets/Scripts/MoveToPoints/MovePoint.cs
@@ -3,14 +3,15 @@ using UnityEngine;
 public class MovePoint : MonoBehaviour
 {
     public int index;
+    private PlayerMovement _playerMovement;
     void Start()
     {
-        
+        _playerMovement = GameObject.Find("/Player").GetComponent<PlayerMovement>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player") && index != 0 && index != 1)
+        if (other.CompareTag("Player") && _playerMovement.isMovingToTargetPoint)
         {
             //GetComponentInParent<MoveToPoints>().TeleportToPoint(index);
             GetComponentInParent<MoveToPoints>().TargetNextPoint(index);
