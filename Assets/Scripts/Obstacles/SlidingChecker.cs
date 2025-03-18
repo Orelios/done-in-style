cf5f0629082c4c20ba2145cedf205ada23270817
SCDN01 <116152025+SCDN01@users.noreply.github.com>
2025-03-18 09:34:55 +0800

    Make sliding indicator have two start restart points

--- Assets/Scripts/Obstacles/SlidingChecker.cs
+++ Assets/Scripts/Obstacles/SlidingChecker.cs
@@ -3,18 +3,20 @@ using UnityEngine;
 public class SlidingChecker : MonoBehaviour
 {
     [SerializeField] private Transform restartPointRight, restartPointLeft;
-    private Transform _closestRespawnPoint; 
+    [SerializeField] private Transform closestRespawnPoint;
     private void OnTriggerStay2D(Collider2D collision)
     {
-        restartPointLeft.position = collision.gameObject.transform.position - restartPointLeft.transform.position;
-        restartPointRight.position = collision.gameObject.transform.position - restartPointRight.transform.position;
-        //_closestRespawnPoint =  
+        float distanceLeft = Vector2.Distance(collision.gameObject.transform.position, restartPointLeft.transform.position); 
+        float distanceRight = Vector2.Distance(collision.gameObject.transform.position, restartPointRight.transform.position);
+
+        if(distanceLeft < distanceRight) { closestRespawnPoint.transform.position = restartPointLeft.transform.position; }
+        else { closestRespawnPoint.transform.position = restartPointRight.transform.position; }
         if (collision.gameObject.TryGetComponent<PlayerTricks>(out var playerTricks))
         {
             if (!playerTricks.IsSliding)
             {
-                collision.gameObject.transform.position = restartPointLeft.position;
-                collision.transform.gameObject.GetComponent<PlayerInvulnerability>().DamagePlayer(); 
+                collision.gameObject.transform.position = closestRespawnPoint.transform.position;
+                collision.transform.gameObject.GetComponent<PlayerInvulnerability>().DamagePlayer();
             }
         }
     }
