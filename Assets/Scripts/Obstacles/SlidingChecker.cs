758853796cf3a0221f0c0fa659c637a380e18a79
Patrick Jose Valenzuela <201901575@iacademy.edu.ph>
2025-03-24 15:56:29 +0800

    Update scene and bug fix

--- Assets/Scripts/Obstacles/SlidingChecker.cs
+++ Assets/Scripts/Obstacles/SlidingChecker.cs
@@ -19,10 +19,10 @@ public class SlidingChecker : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        float distanceLeft = Vector2.Distance(collision.gameObject.transform.position, restartPointLeft.transform.position); 
+        float distanceLeft = Vector2.Distance(collision.gameObject.transform.position, restartPointLeft.transform.position);
         float distanceRight = Vector2.Distance(collision.gameObject.transform.position, restartPointRight.transform.position);
 
-        if(distanceLeft < distanceRight) { closestRespawnPoint.transform.position = restartPointLeft.transform.position; }
+        if (distanceLeft < distanceRight) { closestRespawnPoint.transform.position = restartPointLeft.transform.position; }
         else { closestRespawnPoint.transform.position = restartPointRight.transform.position; }
         if (collision.gameObject.TryGetComponent<PlayerTricks>(out var playerTricks))
         {
@@ -30,13 +30,12 @@ public class SlidingChecker : MonoBehaviour
             {
                 collision.gameObject.transform.position = closestRespawnPoint.transform.position;
                 collision.transform.gameObject.GetComponent<PlayerInvulnerability>().DamagePlayer();
-            }
 
-            if (collision.gameObject.TryGetComponent<Player>(out var player) && !player.Invulnerability.IsInvulnerable)
-            {
-                _scoreCalculator.DecreaseScoreInstant(damage);
-                _rankCalculator.DecreaseStylishPoints(decreasePoints);
-                player.Invulnerability.DamagePlayer();
+                if (collision.gameObject.TryGetComponent<Player>(out var player) && !player.Invulnerability.IsInvulnerable)
+                {
+                    _scoreCalculator.DecreaseScoreInstant(damage);
+                    _rankCalculator.DecreaseStylishPoints(decreasePoints);
+                }
             }
         }
     }
