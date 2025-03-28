52206bc1524fe44cd801f760cbabdeef4f8916c7
SCDN01 <116152025+SCDN01@users.noreply.github.com>
2025-03-28 15:33:41 +0800

    Rework dodge trap indicator

--- Assets/Scripts/TEMP_DodgeTrap.cs
+++ Assets/Scripts/TEMP_DodgeTrap.cs
@@ -1,33 +1,42 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TEMP_DodgeTrap : MonoBehaviour
 {
-    /*
-    [SerializeField]private ScoreCalculator _ScoreCalculator;
+    private GameObject _ScoreCalculator;
     [SerializeField] private int scoreIncrease = 100; 
-    private bool _playerPassed;
+    private bool _playerPassed = false;
 
-    private void Awake()
+    private void Start()
     {
-        _playerPassed = false; 
+        _ScoreCalculator = GameObject.Find("Score System");
     }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<PlayerInvulnerability>(out PlayerInvulnerability playerInvulnerability))
+        {
+            if (playerInvulnerability.IsHit)
+            {
+                _playerPassed = true;
+            }
+        }
+    }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.TryGetComponent<PlayerInvulnerability>(out PlayerInvulnerability playerInvulnerability))
         {
             if (!playerInvulnerability.IsHit && !_playerPassed)
             {
-                _ScoreCalculator.IncreaseScoreInstant(scoreIncrease,
-                    _ScoreCalculator.GetComponent<RankCalculator>().CurrentStylishRank.ScoreMultiplier);
+                _ScoreCalculator.GetComponent<ScoreCalculator>().IncreaseScoreInstant(scoreIncrease,
+                _ScoreCalculator.GetComponent<RankCalculator>().CurrentStylishRank.ScoreMultiplier);
 
                 _ScoreCalculator.GetComponent<RankCalculator>().IncreaseStylishPoints();
 
                 _playerPassed = true;
-
-                gameObject.SetActive(false);
             }
         }
         
     }
-    */
+    
 }
