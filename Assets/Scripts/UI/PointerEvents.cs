b9659401d1a3016dce43100083bb168c5fb06d46
JaRP <100147964+Wooloowooloo@users.noreply.github.com>
2025-04-03 16:44:34 +0800

    Update pause screen

--- Assets/Scripts/UI/PointerEvents.cs
+++ Assets/Scripts/UI/PointerEvents.cs
@@ -11,6 +11,11 @@ public class PointerEvents : MonoBehaviour
     [SerializeField] private Color selectedColor;
     [SerializeField] private Color unselectedColor;
     
+    [Header("Changing Graphic Transparency")]
+    [SerializeField] private Graphic[] graphicsToTransparency;
+    [SerializeField, Range(0, 1f)] private float selectedTransparency;
+    [SerializeField, Range(0, 1f)] private float unselectedTransparency;
+    
     [Header("Changing UGUI Text")]
     [SerializeField] private TextMeshProUGUI textToChange;
     [SerializeField] private float horizontalPositionOnEnter;
@@ -21,6 +26,14 @@ public class PointerEvents : MonoBehaviour
         foreach (var graphic in graphicsToColorize)
         {
             graphic.color = selectedColor;
+        }
+    }
+
+    public void ChangeTransparencyOnPointerEnter()
+    {
+        foreach (var graphic in graphicsToTransparency)
+        {
+            graphic.color = new(graphic.color.r, graphic.color.g, graphic.color.b, selectedTransparency);
         }
     }
 
@@ -36,6 +49,14 @@ public class PointerEvents : MonoBehaviour
         foreach (var graphic in graphicsToColorize)
         {
             graphic.color = unselectedColor;
+        }
+    }
+    
+    public void ChangeTransparencyOnPointerExit()
+    {
+        foreach (var graphic in graphicsToTransparency)
+        {
+            graphic.color = new(graphic.color.r, graphic.color.g, graphic.color.b, unselectedTransparency);
         }
     }
 
