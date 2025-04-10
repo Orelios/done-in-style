53523a8e9df99c563b944950d277d50667f9963d
JaRP <100147964+Wooloowooloo@users.noreply.github.com>
2025-04-10 19:55:49 +0800

    Update game over screen

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [SerializeField] private IntEventChannel healthEventChannel;
     [SerializeField]private int maxHealth;
     public int MaxHealth { get => maxHealth; set => maxHealth = value;  }
     [SerializeField] private EndScreen endScreen; 
@@ -20,50 +21,64 @@ public class PlayerHealth : MonoBehaviour
     {
         playerMovement = GetComponent<PlayerMovement>();
         _currentHealth = maxHealth;
-        playerHealthDisplay.text = $"Health: {_currentHealth: 0}";
+        /*playerHealthDisplay.text = $"Health: {_currentHealth: 0}";
         playerHealthDisplayUpdater.UpdatePlayerHealthBarDisplay(_currentHealth);
-        playerHealthDisplayUpdater.UpdatePlayerPortraitDisplay(_currentHealth);
+        playerHealthDisplayUpdater.UpdatePlayerPortraitDisplay(_currentHealth);*/
         _vfx = GetComponentInChildren<VFXManager>();
         _postProcessing = GetComponent<PostProcessingManager>();
+        
+        healthEventChannel?.Invoke(_currentHealth);
     }
 
     public void DecreaseHealth() 
     {
         AudioManager.instance.PlayOneShot(FMODEvents.instance.PlayerHurt, this.transform.position);
         _vfx.CallHurtVFX();
-        if (_postProcessing.ChromaticAberrationOn && _postProcessing.chromaticAberration != null)
+        if (_postProcessing.ChromaticAberrationOn)
         {
-            _postProcessing.chromaticAberration.StartChromaticAberration();
+            _postProcessing.chromaticAberration?.StartChromaticAberration();
         }
-        if(_currentHealth != 0) 
+        /*if(_currentHealth != 0) 
         { 
             _currentHealth -= 1;
-            playerHealthDisplayUpdater.UpdatePlayerHealthBarDisplay(_currentHealth);
+            //**playerHealthDisplayUpdater.UpdatePlayerHealthBarDisplay(_currentHealth);
             playerHealthDisplayUpdater.UpdatePlayerPortraitDisplay(_currentHealth);
-            playerHealthDisplay.text = $"Health: {_currentHealth: 0}";
+            playerHealthDisplay.text = $"Health: {_currentHealth: 0}";#2##1#
         } 
         if(_currentHealth <= 0) 
         {
             playerMovement._playerSkatingGround.stop(STOP_MODE.ALLOWFADEOUT);
             playerMovement._playerSkatingAir.stop(STOP_MODE.ALLOWFADEOUT);
-            AudioManager.instance.musicEventInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-            AudioManager.instance.ambienceEventInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            AudioManager.instance.musicEventInstance.stop(STOP_MODE.ALLOWFADEOUT);
+            AudioManager.instance.ambienceEventInstance.stop(STOP_MODE.ALLOWFADEOUT);
             AudioManager.instance.musicEventInstance.release();
             AudioManager.instance.ambienceEventInstance.release();
             endScreen.Toggle(true);
             endScreen.EndScreenText("You ran out of health...");
+        }*/
+        
+        _currentHealth = Mathf.Clamp(_currentHealth - 1, 0, maxHealth);
+        
+        if(_currentHealth <= 0) 
+        {
+            playerMovement._playerSkatingGround.stop(STOP_MODE.ALLOWFADEOUT);
+            playerMovement._playerSkatingAir.stop(STOP_MODE.ALLOWFADEOUT);
         }
+        
+        healthEventChannel?.Invoke(_currentHealth);
     }
     public void IncreaseHealth() 
     { 
-        if (_currentHealth != 0) 
+        /*if (_currentHealth != 0) 
         { 
             _currentHealth += 1;
             playerHealthDisplayUpdater.UpdatePlayerHealthBarDisplay(_currentHealth);
             playerHealthDisplayUpdater.UpdatePlayerPortraitDisplay(_currentHealth);
             playerHealthDisplay.text = $"Health: {_currentHealth: 0}";
-        } 
+        } */
+        
+        _currentHealth = Mathf.Clamp(_currentHealth + 1, 0, maxHealth);
+        
+        healthEventChannel?.Invoke(_currentHealth);
     }
-
-    
 }
--- Assets/Scripts/UI/GameOverScreen.cs
+++ Assets/Scripts/UI/GameOverScreen.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
+using FMOD.Studio;
 
 public class GameOverScreen : MonoBehaviour
 {
+    [SerializeField] private GameObject gameOverScreen;
+    
     public void DetectIfGameOver(int playerHealth)
     {
         if (playerHealth <= 0)
         {
-            Debug.Log("Game Over");
+            AudioManager.instance.musicEventInstance.stop(STOP_MODE.ALLOWFADEOUT);
+            AudioManager.instance.ambienceEventInstance.stop(STOP_MODE.ALLOWFADEOUT);
+            AudioManager.instance.musicEventInstance.release();
+            AudioManager.instance.ambienceEventInstance.release();
+            
+            gameOverScreen.SetActive(true);
+            Time.timeScale = 0;
         }
     }
 }
