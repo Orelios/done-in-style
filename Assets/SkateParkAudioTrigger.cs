2ac2276674c28927ae0af90c4c7229e3d903d353
SCDN01 <116152025+SCDN01@users.noreply.github.com>
2025-04-11 15:40:11 +0800

    Implement title screen music

--- Assets/SkateParkAudioTrigger.cs
+++ Assets/SkateParkAudioTrigger.cs
@@ -3,11 +3,21 @@ using FMOD.Studio;
 
 public class SkateParkAudioTrigger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private bool activateInGameMusic = true;
+    [SerializeField] private bool activateTitleScreenMusic = false; 
     void Start()
     {
-        AudioManager.instance.InitializeMusic(FMODEvents.instance.SkateParkMusic1);
-        AudioManager.instance.InitializeAmbience(FMODEvents.instance.SkateParkAmbience);
+        if (activateInGameMusic)
+        {
+            AudioManager.instance.InitializeMusic(FMODEvents.instance.SkateParkMusic1);
+            AudioManager.instance.InitializeAmbience(FMODEvents.instance.SkateParkAmbience);
+        }
+
+        if (activateTitleScreenMusic)
+        {
+            AudioManager.instance.InitializeMusic(FMODEvents.instance.MainMenuMusic);
+        }
+        
     }
 
     // Update is called once per frame
